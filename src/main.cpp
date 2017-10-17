b675fff951b5b26329f855921fe0408599a172b7
Guillermo Soto <guillermo.soto@utec.edu.pe>
2017-10-16 22:26:29 -0500

    16-10-2017
    Today, we're excited to announce Release for this basic project

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,28 @@
 #include <Arduino.h>
 
 int pinesPWM[6] = {11, 10, 9, 6, 5, 3};
+float mapValues;
+int pinAnalog = 0;
+int baudrate = 9600;
 
 void setup() {
     // put your setup code here, to run once:
-    Serial.begin(9600);
+    Serial.begin(baudrate);
     Serial.println("Inicializando...");
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    int potValues = analogRead(0);
+    float potValues = analogRead(pinAnalog);
     /* analog value of 10 bits (0 to 1023) */
     /* digital value of 8 bits (0 to 255) */
     mapValues = map(potValues, 0, 1023, 0, 255);
-    for (int i = 0; i < length(pinesPWM); i++) {
+    for (int i = 0; i < sizeof(pinesPWM); i++) {
       /* code */
-      analogWrite(pin, mapValues);
+      analogWrite(pinesPWM[i], mapValues);
+      Serial.println("pines de senal PWM: ");
+      Serial.print(pinesPWM[i]);
+      Serial.println("valor rango 0 - 255 (0V - 5V): ");
+      Serial.print(mapValues);
     }
-
-
 }
